40594a585d5ef0a9a734e5a58c1ab5584ef2d435
Yo Kamada <bumpfuji10@gmail.com>
2025-05-03 18:47:59 +0900

    不要なコメントを削除し、コードを整理

--- src/app/articles/page.tsx
+++ src/app/articles/page.tsx
@@ -1,18 +1,9 @@
-import { getArticles } from "./../../../lib/api";  // axiosのクライアントからimport
+import { getArticles } from "./../../../lib/api";
 import Link from "next/link";
 
 export default async function ArticlesPage() {
   const data = await getArticles();
   const articles = data.contents;
-  // structure
-  // {
-  //   "id": "a5h-gbtl6k",
-  //   "title": "記事タイトル",
-  //   "createdAt": "2025-02-02T09:10:06.611Z",
-  //   "updatedAt": "2025-02-25T04:28:44.463Z",
-  //   "publishedAt": "2025-02-25T03:31:35.271Z",
-  //   "revisedAt": "2025-02-25T04:28:44.463Z"
-  // }
   return (
     <main style={{ padding: "20px" }}>
       <h1>記事一覧</h1>
