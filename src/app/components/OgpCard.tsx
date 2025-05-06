1b96dfb4fa0770095e6d4092f11a66f4a37e2379
Yo Kamada <bumpfuji10@gmail.com>
2025-05-06 20:02:37 +0900

    スタイルを適用するためにOgpCardコンポーネントをリファクタリングし、SCSSモジュールを追加

--- src/app/components/OgpCard.tsx
+++ src/app/components/OgpCard.tsx
@@ -1,3 +1,5 @@
+import ogpCardStyles from './ogpCard.module.scss';
+
 type OgpData = {
   title?: string;
   description?: string;
@@ -7,13 +9,11 @@ type OgpData = {
 
 export default function OgpCard({ title, description, image, url }: OgpData) {
 return (
-    <div className="ogp-card">
-      <a href={url} target="_blank" rel="noopener noreferrer" style={{
-        borderRadius: '8px',
-      }}>
-        <div style={{ margin: '10px 0', display: 'flex', border: '1px solid #374151', borderRadius: '8px', overflow: 'hidden', backgroundColor: '#1F2937' }}>
-          {image && <img src={image} alt="OGP Image" width="300" />}
-          <div className="ogp-card__content" style={{ padding: '10px', backgroundColor: '#111827', color: 'white', width: '100%' }}>
+    <div className="ogpCard">
+      <a className={ogpCardStyles.ogpCard__cardLink} href={url} target="_blank" rel="noopener noreferrer">
+        <div className={ogpCardStyles.ogpCard__content}>
+          {image && <img className="ogpCard__leftImage" src={image} alt="OGP Image" width="300" />}
+          <div className={ogpCardStyles.ogpCard__rightText}>
             <h4>{title}</h4>
             <p>{description}</p>
           </div>
